ba184e1ff3d5b9cc8e1db4b19674f71444bead86
Bigyan Adhikari <139118193+itsAdk07@users.noreply.github.com>
2024-01-10 16:05:55 +0545

    Update Lab 5 Vertex Cover.cpp

--- DAA/Lab 5 Vertex Cover.cpp
+++ DAA/Lab 5 Vertex Cover.cpp
@@ -4,7 +4,6 @@
 
 using namespace std;
 
-// Graph representation using adjacency list
 class Graph {
 public:
     int V;
@@ -21,11 +20,9 @@ public:
     }
 };
 
-// Greedy algorithm for Vertex Cover Problem
 unordered_set<int> vertexCover(Graph& graph) {
     unordered_set<int> cover;
 
-    // Initialize the set of uncovered edges
     vector<bool> visited(graph.V, false);
     for (int u = 0; u < graph.V; u++) {
         for (int v : graph.adjList[u]) {
@@ -40,7 +37,6 @@ unordered_set<int> vertexCover(Graph& graph) {
     return cover;
 }
 
-// Driver code
 int main() {
     Graph graph(4);
     graph.addEdge(0, 1);
