1f03fb969a87e80fbfd6bf77bd98c8d37cd34993
bigyan <bigyanadhikari07@gmail.com>
2024-05-29 09:30:57 +0545

    l

---
+++ NCC/NCCProj/NCCProj/Models/DataDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NCCProj.Models
+{
+    public class DataDbContext : DbContext
+    {
+        public DataDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<users> Users { get; set; }
+    }
+}
---
+++ NCC/NCCProj/NCCProj/Models/Users.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NCCProj.Models
+{
+    public class users
+    {
+        [Key]
+        public int id { get; set; }
+        public string name { get; set; }
+        public string contact { get; set; }
+    }
+}
